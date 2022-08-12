c4b0f25cf4fa33706fe499849fb2017423ba90ae
Maxi <mcortes.na@gmail.com>
2022-08-12 12:46:41 -0300

    popups dinámicos, dos estilos

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,27 @@
-//Inicializar funciones de a-frame
-if(document.querySelector('html').classList.contains('aframe')){
+//Inicializar funciones de a-frame y popups en el home
+if(document.querySelector('html').classList.contains('home')){
     //Wanderers
     const WDR = new Wanderers('.wanderer');
     //Popups
-    setupGlobalLoaderListeners(); //TODO: separar entre loaders locales y globales
-    setupCloseModalListeners();
+    setupGlobalLoaderListeners();
+}
+
+//TODO: Agrupar estas funciones en un componente/clase Popup
+
+function setupLocalClosingListeners(wrapper) {
+    const contentClosers = wrapper.querySelectorAll('.js-close-modal');
+    for(let closer of contentClosers){
+        closer.addEventListener('click', function localContentCloser(event) {
+            event.preventDefault();
+            let element = event.target;
+            element.removeEventListener('click', localContentCloser);
+            //Eliminar contenido actual
+            hideModal(wrapper).then(() => {
+                wrapper.remove();
+            });
+        });
+    }
+
 }
 
 function setupGlobalLoaderListeners () {
@@ -21,100 +38,110 @@ function setupGlobalLoaderListeners () {
     }
 }
 
-function setupLocalLoaderListeners () {
+function setupLocalLoaderListeners (wrapper) {
     //Loaders que dejan de existir al cargar un nuevo contenido
-    const contentLoaders = document.querySelectorAll('.popup-content-wrapper .js-load-content');
+    const contentLoaders = wrapper.querySelectorAll('.js-load-content');
     for(let loader of contentLoaders){
+
         loader.addEventListener('click', function localContentLoader(event) {
             event.preventDefault();
             let element = event.target;
             element.removeEventListener('click', localContentLoader)
             let link = element.getAttribute('href');
-            loadContent(link);
-        })
+
+            //Eliminar contenido actual, y después mostrar el nuevo contenido
+            hideModal(wrapper).then(() => {
+                wrapper.remove();
+                loadContent(link);
+            });
+        });
     }
 }
 
-function setupCloseModalListeners(){
-    const modalClosers = document.querySelectorAll('.js-close-modal');
-    for ( let closer of modalClosers ){
-        closer.addEventListener('click', (event) => {
-            event.preventDefault();
-            closePopup();
+function hideModal(wrapper){
+    const popupBody = wrapper.querySelector('#popup-main');
+
+    return new Promise((resolve, reject) => {
+
+        popupBody.addEventListener('transitionend', () => {
+            resolve();
         })
-    }
-}
 
-//TODO: Agrupar estas funciones en un componente/clase Popup
-
-function closePopup(){
-    const popupBody = document.querySelector('#popup-body');
-    const contentWrapper = document.querySelector('.popup-content-wrapper');
+        if(popupBody.classList.contains('view-fade-in--visible')){
+            popupBody.classList.remove('view-fade-in--visible');
+        }
     
-    if(popupBody.classList.contains('view-fade-in--visible')){
-        popupBody.classList.remove('view-fade-in--visible');
-    }
-
-    if(popupBody.classList.contains('view-togglable-pointer--active')){
-        popupBody.classList.remove('view-togglable-pointer--active');
-    }
-
-    if(contentWrapper.classList.contains('view-fade-in--visible')){
-        contentWrapper.classList.remove('view-fade-in--visible');
-    }
+        if(popupBody.classList.contains('view-togglable-pointer-events--active')){
+            popupBody.classList.remove('view-togglable-pointer-events--active');
+        }
+    });
 }
 
-function openPopup() {
-    const popupBody = document.querySelector('#popup-body');
+function showModal(wrapper) {
+    const popupBody = wrapper.querySelector('#popup-main');
+
+    popupBody.getBoundingClientRect(); //Forzar reflow
+
     if(! popupBody.classList.contains('view-fade-in--visible')){
         popupBody.classList.add('view-fade-in--visible');
     }
-    if(! popupBody.classList.contains('view-togglable-pointer--active')){
-        popupBody.classList.add('view-togglable-pointer--active');
+    if(! popupBody.classList.contains('view-togglable-pointer-events--active')){
+        popupBody.classList.add('view-togglable-pointer-events--active');
     }
 }
 
 
 function loadContent(url) {
+    fetch('contents/'+url).then((response) => {
+        console.log('content promise done');
+        return response.text();
+    })
+    .then((content) => {
+        const modal = document.createElement('div');
+        modal.innerHTML = content;
+        const body = document.querySelector('body');
+        body.append(modal); //agregar el contenido
 
-    openPopup();
-    const contentWrapper = document.querySelector('.popup-content-wrapper');
+        showModal(modal);
+        setupLocalLoaderListeners(modal);
+        setupLocalClosingListeners(modal);
+    });
+
+
 
     // Manage content visibility
+    // console.log('loading content...');
 
-    //BUG: No abre el popup cuando toco un botón global
-    console.log('loading content...');
+    // const transitionPromise = new Promise((resolve) => {
+    //     //If the popup already has content, wait until it has faded out before resolving:
+    //     if(contentWrapper.classList.contains('view-fade-in--visible')){
+    //         console.log('popup has content');
+    //         contentWrapper.classList.remove('view-fade-in--visible');
+    //         contentWrapper.addEventListener('transitionend', function animationendListener() {
+    //             contentWrapper.removeEventListener('transitionend', animationendListener);
+    //             contentWrapper.classList.add('view-fade-in--visible');
+    //             resolve();
+    //         });
+    //     }
+    //     //Else, resolve immediately:
+    //     else {
+    //         console.log('popup does not have content');
+    //         contentWrapper.classList.add('view-fade-in--visible');
+    //         resolve();
+    //     }
+    //     console.log('transition promise done');
+    // });
 
-    const transitionPromise = new Promise((resolve) => {
-        //If the popup already has content, wait until it has faded out before resolving:
-        if(contentWrapper.classList.contains('view-fade-in--visible')){
-            console.log('popup has content');
-            contentWrapper.classList.remove('view-fade-in--visible');
-            contentWrapper.addEventListener('transitionend', function animationendListener() {
-                contentWrapper.removeEventListener('transitionend', animationendListener);
-                contentWrapper.classList.add('view-fade-in--visible');
-                resolve();
-            });
-        }
-        //Else, resolve immediately:
-        else {
-            console.log('popup does not have content');
-            contentWrapper.classList.add('view-fade-in--visible');
-            resolve();
-        }
-        console.log('transition promise done');
-    });
-
-    Promise.all([
-        fetch('contents/'+url).then((response) => {
-            console.log('content promise done');
-            return response.text();
-        }),
-        transitionPromise
-    ])
-    .then((returnedValues) => {
-        const content = returnedValues[0];
-        contentWrapper.innerHTML = content;
-        setupLocalLoaderListeners();
-    });
+    // Promise.all([
+    //     fetch('contents/'+url).then((response) => {
+    //         console.log('content promise done');
+    //         return response.text();
+    //     }),
+    //     transitionPromise
+    // ])
+    // .then((returnedValues) => {
+    //     const content = returnedValues[0];
+    //     contentWrapper.innerHTML = content;
+    //     setupLocalLoaderListeners();
+    // });
 }
--- main.js
+++ main.js
@@ -11,7 +11,37 @@ const files = require('./fileFunctions'); //Funciones para simplificar la creaci
 let styleRules = sass.compile(__dirname + '/src/sass/main.sass');
 files.writeFile(__dirname + '/dist/css/styles.css', styleRules.css);
 
-//Renderizar HTML
+//Renderizar páginas HTML
 const pageSrcPath = __dirname + '/src/pug/pages/';
-
 files.writeFile(__dirname + '/dist/index.html', pug.renderFile(pageSrcPath + 'index.pug'));
+
+//Renderizar modales HTML
+const modalSrcPath = __dirname + '/src/pug/modals/';
+const outputPath = __dirname + '/dist/contents/';
+const modalFilenames = [
+    'mariela-A',
+    'mariela-A1',
+    'mariela-A2',
+    'mariela-A3',
+    'mariela-A4',
+    'mariela-B',
+    'mariela-B1',
+    'mariela-C',
+    'mariela-C1',
+    'mariela-C2',
+    'mariela-D',
+    'mariela-D1',
+    'mariela-E',
+    'mariela-E1',
+    'mariela-E2',
+    'ana-01',
+    'ana-02',
+    'ana-03',
+    'ana-04',
+    'ana-05',
+    'ana-06',
+];
+
+for ( let filename of modalFilenames ) {
+    files.writeFile(outputPath + filename + '.html', pug.renderFile(modalSrcPath + filename + '.pug'));
+}
