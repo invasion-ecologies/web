1bd39e67403ed64a6091edeb92db4898c8c8e000
Maxi <mcortes.na@gmail.com>
2022-10-04 10:31:50 -0300

    encapsular funcionamiento modales en una clase

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,4 @@
 //Funciones de modal inicializadas en modal.js
-//TODO: refactorizar modales como ¿objetos?
 //¿Considerar al DOM del index como un solo objeto mutado por las funciones de modales?
 
 //Inicializar objeto global app
@@ -16,28 +15,34 @@ const utils = {
 
 //Wanderers
 App.wanderers = new Wanderers('.wanderer');
+
 //Asignar event listeners a las figuras para abrir los modales
-setupGlobalLoaderListeners();
-
-App.aframeScene.addEventListener('loaded', ()=>{
-    //Esperar 1 segundo más y liberar overlay
-    setTimeout(() => {
-        App.StateComponents.changeState(App.preloader, 'overlay', 'default');
-    }, 1000);
-});
-
-
-
-// Preloader
-//TODO: Agregar un método .add() a StateComponents, para poder agregar componentes en distintas secciones
-
-App.StateComponents = new StateComponents({
-    'overlay': {
-        stateClasses: {
-        'default': ['view-fade-in', 'view-togglable-pointer-events'],
-        'active': ['view-fade-in--visible', 'view-togglable-pointer-events--active'],
-        }
+App.modals = new Modals({
+    modalSelector: '#popup-main',
+    afterInsertFunction: () => {
+        App.StateComponents.initializeAllComponentsOnDOM();
+    },
+    showingFunction: (popup) => {
+        App.StateComponents.changeState(popup, 'overlay', 'active');
+    },
+    hidingFunction: (popup) => {
+        App.StateComponents.changeState(popup, 'overlay', 'default');
     }
 });
 
+App.aframeScene.addEventListener('loaded', () => {
+    //Esperar 1 segundo más y liberar overlay
+    setTimeout(() => {
+        App.StateComponents.changeState(App.preloader, 'overlay', 'default');
+    });
+});
 
+
+App.StateComponents = new StateComponents();
+
+App.StateComponents.add('overlay', {
+    stateClasses: {
+        'default': ['view-fade-in', 'view-togglable-pointer-events'],
+        'active': ['view-fade-in--visible', 'view-togglable-pointer-events--active'],
+    }
+});
