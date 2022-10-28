287609e4ac35d8b70a5787f50029f2140124e80d
Maxi <mcortes.na@gmail.com>
2022-10-28 15:41:06 -0300

    corregir preventDefault en js-close-modal

--- dist/js/index.js
+++ dist/js/index.js
@@ -99,11 +99,11 @@ const App = {
                 selector: '.js-close-modal',
                 type: 'click',
                 callbackFunction: (e) => {
-                    e.preventDefault();
-                    const target = e.target;
-                    const modalName = target.dataset.modalName;
                     //Do something only if we click exactly on the target
                     if(target.classList.contains('js-close-modal')){
+                        e.preventDefault();
+                        const target = e.target;
+                        const modalName = target.dataset.modalName;
                         App.modalManager.hideModal(modalName);
                     }
                 },
